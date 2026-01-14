30cad6f6e4da9adf6b98a0f84b00b370ae1a361c
Abhijit Shyamal <abhijitshyamal005@gmail.com>
2026-01-14 09:52:19 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/register' element={<Register />} />
+
           <Route path='/' element={<PrivateRoutes />}>
             <Route path='/home' element={<DocumentHome/>} />
             <Route path='/edit/:id' element={<EditDocument/>} />
